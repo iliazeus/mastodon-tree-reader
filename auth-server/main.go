c524558d7010c90b01120f2c33f52193e3aa6623
Ilia Pozdnyakov <iliazeus@proton.me>
2024-10-29 12:12:32 +0500

    rewrite server with sync types

--- auth-server/main.go
+++ auth-server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"io"
@@ -9,8 +8,160 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
+	"regexp"
+	"sync"
+	"time"
 )
+
+type j = map[string]any
+
+func main() {
+	filename := os.Getenv("APPS_FILE")
+	if filename == "" {
+		filename = "apps.json"
+	}
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	if listenAddr == "" {
+		listenAddr = ":8080"
+	}
+
+	cache := NewAppCache(filename)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /app", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+
+		inst, err := ParseInstanceHost(query.Get("instance"))
+		if err != nil {
+			slog.Warn("bad request", "error", err)
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(j{"error": err.Error()})
+			return
+		}
+
+		app, err := cache.LookupOrRegister(inst)
+		if err != nil {
+			slog.Warn("internal error", "instance", inst, "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(j{"error": err.Error()})
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(app)
+	})
+
+	slog.Info("listening", "addr", listenAddr)
+	http.ListenAndServe(listenAddr, mux)
+}
+
+type AppCache struct {
+	filename string
+	mu       *sync.Mutex
+	apps     map[InstanceHost]func() (*App, error)
+}
+
+func NewAppCache(filename string) *AppCache {
+	c := &AppCache{filename, &sync.Mutex{}, map[InstanceHost]func() (*App, error){}}
+	c.load()
+	return c
+}
+
+func (c *AppCache) LookupOrRegister(inst InstanceHost) (*App, error) {
+	c.mu.Lock()
+
+	f, ok := c.apps[inst]
+	if ok {
+		slog.Info("found cached entry", "instance", inst)
+	} else {
+		slog.Info("no cached entry, registering app", "instance", inst)
+		f = sync.OnceValues(func() (*App, error) {
+			app, err := RegisterApp(inst)
+			if err == nil {
+				go c.store()
+			} else {
+				go c.deleteAfter(inst, 1*time.Second)
+			}
+			return app, err
+		})
+		c.apps[inst] = f
+	}
+
+	c.mu.Unlock()
+	return f()
+}
+
+func (c *AppCache) deleteAfter(inst InstanceHost, d time.Duration) {
+	<-time.After(d)
+	c.mu.Lock()
+	delete(c.apps, inst)
+	c.mu.Unlock()
+}
+
+func (c *AppCache) load() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	slog.Info("loading stored cache", "filename", c.filename)
+
+	appsJson := make(map[string]*App)
+
+	bytes, err := os.ReadFile(c.filename)
+	if err == nil {
+		err = json.Unmarshal(bytes, &appsJson)
+	}
+	if err != nil {
+		slog.Warn("error loading app cache", "error", err)
+	} else {
+		slog.Info("loaded stored cache", "len", len(appsJson))
+		for k, v := range appsJson {
+			c.apps[InstanceHost(k)] = func() (*App, error) {
+				return v, nil
+			}
+			slog.Info("loaded stored cache entry", "instance", k)
+		}
+	}
+}
+
+func (c *AppCache) store() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	slog.Info("storing app cache", "len", len(c.apps))
+
+	appsJson := make(map[string]*App)
+	for k, v := range c.apps {
+		app, err := v()
+		if err == nil {
+			appsJson[string(k)] = app
+		}
+	}
+
+	bytes, err := json.MarshalIndent(appsJson, "", "  ")
+	if err == nil {
+		err = os.WriteFile(c.filename, bytes, 0666)
+	}
+	if err != nil {
+		slog.Warn("error storing app cache", "error", err)
+	} else {
+		slog.Info("stored app cache")
+	}
+}
+
+type InstanceHost string
+
+var instanseHostRegexp = regexp.MustCompile("^(?:https?://)?(.*?)(?::80|:443)?(?:/.*)?$")
+var ErrInvalidInstanceHost = errors.New("invalid instance host")
+
+func ParseInstanceHost(h string) (InstanceHost, error) {
+	m := instanseHostRegexp.FindStringSubmatch(h)
+	if m != nil && m[1] != "" {
+		return InstanceHost(m[1]), nil
+	} else {
+		return InstanceHost(""), ErrInvalidInstanceHost
+	}
+}
 
 // https://docs.joinmastodon.org/entities/Application/#CredentialApplication
 type App struct {
@@ -20,131 +171,12 @@ type App struct {
 	RedirectUris          []string `json:"redirect_uris"`
 	ClientId              string   `json:"client_id"`
 	ClientSecret          string   `json:"client_secret"`
-	ClientSecretExpiresAt string   `json:"client_secret_expires_at"`
+	ClientSecretExpiresAt string   `json:"client_secret_expires_at,omitempty"`
 }
 
-type j = map[string]any
-
-func main() {
-	ctx := context.Background()
-
-	filename := os.Getenv("APPS_FILE")
-	if filename == "" {
-		filename = "apps.json"
-	}
-
-	cache := NewAppCache(filename)
-	go cache.Run(ctx)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /app", func(w http.ResponseWriter, r *http.Request) {
-		query := r.URL.Query()
-
-		instance := query.Get("instance")
-		instance, _ = strings.CutPrefix(instance, "http://")
-		instance, _ = strings.CutPrefix(instance, "https://")
-		instance, _ = strings.CutSuffix(instance, "/")
-		if instance == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(j{"error": "instance parameter not passed"})
-			return
-		}
-
-		app, err := cache.Request(instance)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(j{"error": err})
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(app)
-	})
-
-	listenAddr := os.Getenv("LISTEN_ADDR")
-	if listenAddr == "" {
-		listenAddr = ":8080"
-	}
-
-	slog.Info("starting server", "addr", listenAddr)
-	http.ListenAndServe(listenAddr, mux)
-}
-
-type appCacheRequest struct {
-	instance string
-	result   chan *App
-	err      chan error
-}
-
-type AppCache struct {
-	filename string
-	apps     map[string]*App
-	reqs     chan appCacheRequest
-}
-
-func NewAppCache(filename string) AppCache {
-	slog.Info("reading stored apps", "filename", filename)
-
-	apps := make(map[string]*App)
-	bytes, err := os.ReadFile(filename)
-	if err != nil {
-		slog.Warn("error reading stored apps", "error", err)
-	} else {
-		json.Unmarshal(bytes, &apps)
-	}
-
-	slog.Info("loaded stored apps", "count", len(apps))
-	for instance := range apps {
-		slog.Info("loaded stored app", "instance", instance)
-	}
-
-	return AppCache{
-		filename,
-		apps,
-		make(chan appCacheRequest),
-	}
-}
-
-func (c *AppCache) Request(instance string) (*App, error) {
-	apps := make(chan *App)
-	errs := make(chan error)
-	c.reqs <- appCacheRequest{instance, apps, errs}
-	select {
-	case err := <-errs:
-		return nil, err
-	case app := <-apps:
-		return app, nil
-	}
-}
-
-func (c *AppCache) Run(ctx context.Context) error {
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case req := <-c.reqs:
-			slog := slog.With("instance", req.instance)
-			slog.Info("got request for app")
-			app, err := c.handleRequest(req.instance, slog)
-			if err != nil {
-				slog.Error("error while handling request", "error", err)
-				req.err <- err
-			} else {
-				req.result <- app
-			}
-		}
-	}
-}
-
-func (c *AppCache) handleRequest(instance string, slog *slog.Logger) (*App, error) {
-	app := c.apps[instance]
-	if app != nil {
-		slog.Info("app found in cache")
-		return app, nil
-	}
-
-	slog.Info("no app in cache, requesting from remote")
-	res, err := http.PostForm("https://"+instance+"/api/v1/apps", url.Values{
+func RegisterApp(inst InstanceHost) (*App, error) {
+	var app *App
+	res, err := http.PostForm("https://"+string(inst)+"/api/v1/apps", url.Values{
 		"client_name":   {"Tree Reader"},
 		"website":       {"https://iliazeus.lol/mastodon-tree-reader/thread.html"},
 		"redirect_uris": {"https://iliazeus.lol/mastodon-tree-reader/thread.html"},
@@ -159,19 +191,5 @@ func (c *AppCache) handleRequest(instance string, slog *slog.Logger) (*App, erro
 	if err == nil {
 		err = json.NewDecoder(res.Body).Decode(&app)
 	}
-	if err != nil {
-		return nil, err
-	}
-
-	slog.Info("got response from remote, storing")
-	c.apps[instance] = app
-	bytes, err := json.Marshal(c.apps)
-	if err == nil {
-		err = os.WriteFile(c.filename, bytes, 0666)
-	}
-	if err != nil {
-		slog.Warn("error writing apps to file", "error", err)
-	}
-
-	return app, nil
+	return app, err
 }
